3a32660b3eadcb85895de031a6c1f5ead9728183
Mauricio Matoma <97911503+matomatsu924@users.noreply.github.com>
2025-03-19 21:38:17 -0500

    Data Eucalyptus, R, plot. Code R

--- RM_PT.R
+++ RM_PT.R
@@ -26,3 +26,32 @@ summary(m)
 plot_model(m,terms=c("Group"), type="eff") + theme_bw()
 
 plot_model(m,terms=c("Group"), type="re") + theme_bw()
+
+
+################################################################################
+
+d <- read.csv("eucalyptus.csv")
+d <- d %>% select(plot,stocking,spp,hgt)
+#View(d)
+
+library(lme4)
+library(lmerTest)
+library(performance)
+
+d$stocking <- factor(d$stocking)
+
+m0 <- lm(hgt~ spp*stocking, data=d)
+step(m0)
+m1 <- lmer(hgt~ spp*stocking + (1|plot), data=d)
+step(m1)
+m2 <- lmer(hgt~ spp+stocking + (1|plot), data=d)
+
+anova(m0,m1,m2)
+compare_performance(m0,m1,m2)
+
+summary(m1)
+
+ranef(m1) #Extraer el itercepto de los efectos aleatorios
+
+
+plot_model(m1,terms=c("spp","stocking"), type="int") + theme_bw()
